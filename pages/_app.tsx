d9d4723c292f7cda94f71c9afb932e9d62eb2aed
Isaack Njama <isaacknjama@proton.me>
2024-10-04 11:38:53 +0300

    feat: add homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import 'tailwindcss/tailwind.css';
 import NavBar from '../components/NavBar';
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [darkMode, setDarkMode] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode) {
+      const isDarkMode = JSON.parse(savedDarkMode);
+      setDarkMode(isDarkMode);
+      document.documentElement.classList.toggle('dark', isDarkMode);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    document.documentElement.classList.toggle('dark', newDarkMode);
+    localStorage.setItem('darkMode', JSON.stringify(newDarkMode));
+  };
+
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Component {...pageProps} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </>
   );
 }
