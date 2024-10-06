771e8652addfba60a152ce0353d4ce8813acfe47
Isaack Njama <isaacknjama@proton.me>
2024-10-06 12:46:09 +0300

    feat: define spinner
    chore: add dependency version file

---
+++ components/Spinner.tsx
@@ -0,0 +1,24 @@
+const Spinner = () => (
+  <div className='spinner'>
+    <style jsx>{`
+      .spinner {
+        border: 4px solid rgba(0, 0, 0, 0.1);
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        border-left-color: #22c55e;
+        animation: spin 1s ease infinite;
+      }
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `}</style>
+  </div>
+);
+
+export default Spinner;
