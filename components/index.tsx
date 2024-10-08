b5cfb2e8b29d71f5dda5751d66ed7c14e9e161b9
Isaack Njama <isaacknjama@proton.me>
2024-10-08 10:43:35 +0300

    feat: define dashboard view

---
+++ components/index.tsx
@@ -0,0 +1,13 @@
+import CreateOrderForm from "./CreateOrderForm";
+import LoginForm from "./LoginForm";
+import Spinner from "./Spinner";
+import Tooltip from "./Tooltip";
+import Dashboard from "./Dashboard";
+
+export {
+  CreateOrderForm,
+  Dashboard,
+  LoginForm,
+  Spinner,
+  Tooltip,
+};
