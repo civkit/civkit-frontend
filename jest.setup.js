825e440bda54f72e5b929ed377cbc3e0759d05df
Isaack Njama <isaacknjama@proton.me>
2024-11-04 10:41:51 +0300

    test: fix encoder and decoder

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 require('@testing-library/jest-dom');
+
+// Polyfill TextEncoder and TextDecoder for Node.js environment
+if (typeof TextEncoder === 'undefined') {
+  global.TextEncoder = require('util').TextEncoder;
+}
+
+if (typeof TextDecoder === 'undefined') {
+  global.TextDecoder = require('util').TextDecoder;
+}
