254e0b353805f6552040bfe444628363ee02eb88
MarketMadi <market_madi1@protonmail.com>
2024-11-20 09:13:48 +0530

    resume order v9 - v8 worked for maker

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1320,9 +1320,12 @@ const Dashboard: React.FC<{
                                       } catch (error) {
                                         console.error('Failed to fetch invoice data');
                                       }
+                                    } else {
+                                      // Taker flow
+                                      handleTakeOrder(order);
                                     }
                                   }}
-                                  className='focus:shadow-outline mt-2 rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600 focus:outline-none'
+                                  className='inline-flex items-center justify-center px-3 py-1 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
                                 >
                                   Resume Order
                                 </button>
